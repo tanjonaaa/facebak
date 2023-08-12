335c361b3733f7bfcecf45e86617fb9a0939c004
”ToavinaMaria” <ToavinaMaria>
2023-08-12 11:27:35 +0300

    fix: SignUp file

--- src/components/CommentPostModal/Route/Route.jsx
+++ src/components/CommentPostModal/Route/Route.jsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes as Router , Route} from "react-router-dom"
 import News from "../../../pages/Home/News"
+import SignUp from "../../../pages/SignUp/SignUp";
 export default function Routes(){
     return (
         <BrowserRouter>
             <Router>
                 <Route path="/" Component={News}/>
+                <Route path="/sign-up" Component={SignUp}/>
             </Router>
         </BrowserRouter>
     )
