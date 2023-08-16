0a80195ad22dac90a99be8e3e58d16c274ad955d
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-08-16 19:35:52 +0300

    feat: apply context

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import {NewsFeed} from "./pages/NewsFeed";
 import {Navbar} from './components/Navbar';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ClientProvider from "./components/provider";
 
 function App() {
   return (
     <BrowserRouter>
-        <Navbar />
-        <div className='h-12 pt-1'/>
-        <Routes>
-            <Route path="/" Component={NewsFeed}/>
-        </Routes>
+        <ClientProvider>
+            <Navbar />
+            <div className='h-12 pt-1'/>
+            <Routes>
+                <Route path="/" Component={NewsFeed}/>
+            </Routes>
+        </ClientProvider>
     </BrowserRouter>
   );
 }
