a925363a9d15fc05fbf311ba4099db51caed27e5
”ToavinaMaria” <ToavinaMaria>
2023-08-18 15:12:38 +0300

    feat : SignUp

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { HiOutlineEye, HiOutlineEyeOff } from 'react-icons/hi';
-
+import axios from "axios";
 function SignUp() {
         const [showPassword, setShowPassword] = useState(false);
 
@@ -14,9 +14,36 @@ function SignUp() {
         setShowConfirmPassword(!showConfirmPassword);
     };
 
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+        username: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios.post('/api/signup', formData)
+            .then((response) => {
+                // Handle success
+                console.log(response.data);
+            })
+            .catch((error) => {
+                // Handle error
+                console.error(error);
+            });
+    };
+
+
+
     return(
         <>
-            <section className="bg-rich-black dark:bg-prussian-blue">
+            <section onClick={handleSubmit} className="bg-rich-black dark:bg-prussian-blue">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="/#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-prussian-blue">
                         <img className="w-12 h-12 mr-2" src="/logo1.png" alt="logo"/>
@@ -29,7 +56,7 @@ function SignUp() {
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-rich-black border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                    <input type="email" value={formData.email} onChange={handleChange} name="email" id="email" className="bg-rich-black border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -58,7 +85,14 @@ function SignUp() {
                                         <label htmlFor="terms" className="font-light text-gray-500 dark:text-white">I accept the <a className="font-medium text-white hover:underline dark:text-white" href="#">Terms and Conditions</a></label>
                                     </div>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-prussian-blue focus:ring-4 focus:outline-none focus:bg-prussian-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-prussian-blue dark:focus:bg-prussian-blue">Create an account</button>
+                                <div className="bg-picton-blue rounded-lg border-none">
+                                <button
+                                    type="submit" onClick={handleSubmit}
+                                    className="w-full bg-cerulean border-none hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700"
+                                >
+                                    Create an account
+                                </button>
+                                </div>
 
                                 <p className="text-sm font-light text-white dark:text-white">
                                     Already have an account? <a href="/#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
