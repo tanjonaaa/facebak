724e29b29a2fa9377486164441732051fa8607c4
tanjonaaa <hei.tanjona@gmail.com>
2023-08-25 14:50:12 +0300

    feat: added ClientContext and link to signup on navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Navbar = () => {
     return (
         <header className="flex p-2.5 py-3.5 fixed w-full bg-white border-b z-50">
             <div className="flex justify-between w-full">
-                <a href="/#">Facebak</a>
+                <a href="/">Facebak</a>
+                <Link to="/sign-up">Inscription</Link>
                 <div>
                     <a className="link-to-profile" href="/link-to-user-profile">
                         profile image
