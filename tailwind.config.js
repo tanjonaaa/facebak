2510428ebe1631902753d4a2f7464edb16fb7957
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-08-25 19:03:45 +0300

    refactor: clean project file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx,html}"
   ],
   theme: {
-    colors: {"rich-black": "#00171fff",
-      "prussian-blue": "#003459ff",
-      "cerulean": "#007ea7ff",
-      "picton-blue": "#00a8e8ff",
-      "white" : "#ffffffff",
+    extend: {
+      colors: {
+        "rich-black": "#00171fff",
+        "prussian-blue": "#003459ff",
+        "cerulean": "#007ea7ff",
+        "picton-blue": "#00a8e8ff",
+        "white" : "#ffffffff",
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
