3fd94c0c1d088ad948163d0cc682402c2234a163
Abi Ny Fiderana <121378373+Abi-Nf@users.noreply.github.com>
2023-08-27 18:39:56 +0300

    feat: update EmailInput.jsx

--- src/pages/SignUp/EmailInput.jsx
+++ src/pages/SignUp/EmailInput.jsx
@@ -12,16 +12,9 @@ const EmailInput = ({ value, onChange }) => {
                 onChange={onChange}
                 id="email"
                 name="email"
-                className="
-                bg-white 
-                border border-red-700 
-                text-gray-900 sm:text-sm 
-                rounded-lg 
-                focus:border-primary-600 
-                block 
-                w-full 
-                p-2.5 
-                dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-rich-black dark:focus:ring-blue-500 dark:focus:border-white"
+                className="bg-white border border-red-700 text-gray-900 sm:text-sm rounded-lg focus:border-primary-600 
+                block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+                dark:text-rich-black dark:focus:ring-blue-500 dark:focus:border-white"
                 placeholder="name@company.com"
                 required=""
             />
