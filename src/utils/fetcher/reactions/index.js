684f864cbd64b6c089a2eaaf7ef1bce62c30f92c
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-08-27 16:54:19 +0300

    refactor: re-arrange profile branch

--- src/utils/fetcher/reactions/index.js
+++ src/utils/fetcher/reactions/index.js
@@ -3,20 +3,20 @@ const host = "http://localhost:8080"; // default on local
 
 const getReaction = async (postUuid) => {
     return (await axios.get(
-        host.concat( "/posts/", postUuid, "/reactions" )
+        `${host}/posts/${postUuid}/reactions`,
     )).data
 }
 
 const sendReactionToPost = async (postUuid,data) => {
     return (await axios.post(
-        host.concat( "/posts/", postUuid, "/reactions" ),
+        `${host}/posts/${postUuid}/reactions`,
         data,
     )).data;
 }
 
 const deleteAReaction = async (postUuid,data) => {
     return (await axios.delete(
-        host.concat( "/posts/", postUuid, "/reactions" ),
+        `${host}/posts/${postUuid}/reactions`,
         data
     )).data
 }
